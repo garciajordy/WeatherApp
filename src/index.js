2f571dd0101af3015d84c14b09f7aedc19a0077a
garciajordy <jordygarcia0119@gmail.com>
2021-05-10 19:34:16 +0300

    Solve all linter errors

--- src/index.js
+++ src/index.js
@@ -1,25 +1,23 @@
-import "../style/style.scss"
+const getData = (city) => {
+  const data = fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=9ce157141208c7f7d8b28e7e79719ed2&units=metric`).then((res) => res.json());
+  return data;
+};
 
-const getData = function(city) {
-    let data = fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=9ce157141208c7f7d8b28e7e79719ed2&units=metric`).then(function (res) {
-        return res.json()
-    })
-    return data
-}
 function weather() {
-    let city = document.getElementById("city").value
-    getData(city).then(function (data) {
-        let temp = document.getElementById("temp");
-        let minTemp = document.getElementById("min-temp");
-        let maxTemp = document.getElementById("max-temp");
-        let city = document.getElementById("city-1");
-        let weatherCast = document.getElementById("weather-cast");
-        city.innerHTML = data.name
-        weatherCast.innerHTML = data.weather[0].description
-        temp.innerHTML = `Temp = ${data.main.temp} Celsius`
-        minTemp.innerHTML = `Min-temp = ${data.main.temp_min} Celsius`
-        maxTemp.innerHTML = `Max-temp = ${data.main.temp_max} Celsius`
-        console.log(data.main)
-    })
+  const city = document.getElementById('city').value;
+  getData(city).then((data) => {
+    const temp = document.getElementById('temp');
+    const minTemp1 = document.getElementById('min-temp');
+    const maxTemp = document.getElementById('max-temp');
+    const city1 = document.getElementById('city-1');
+    const weatherCast = document.getElementById('weather-cast');
+    city1.innerHTML = data.name;
+    weatherCast.innerHTML = data.weather[0].description;
+    temp.innerHTML = `Temp = ${data.main.temp} Celsius`;
+    minTemp1.innerHTML = `Min-temp = ${data.main.temp_min} Celsius`;
+    maxTemp.innerHTML = `Max-temp = ${data.main.temp_max} Celsius`;
+  });
 }
-document.getElementById("btn").addEventListener("click",weather)
+
+const btn = document.getElementById('btn');
+btn.addEventListener('click', weather);
